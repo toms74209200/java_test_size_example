1ca91673a26ef206a4a24ac952d3e91b09857e1e
toms74209200 <motomotomato@gmail.com>
2024-10-20 16:32:10 +0900

    :sparkles: Add project

---
+++ src/integration-test/java/com/example/ExampleIT.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ExampleIT {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}

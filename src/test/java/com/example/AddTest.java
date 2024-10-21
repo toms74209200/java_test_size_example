4cd393d5c40f0b889277e4a85053af561ca3f1c4
toms74209200 <motomotomato@gmail.com>
2024-10-21 21:15:00 +0900

    CI check

---
+++ src/test/java/com/example/AddTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AddTest {
+    @Test
+    public void testAdd() {
+        assertEquals(1+2, 3);
+    }
+}
